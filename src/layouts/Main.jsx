bb59b94440228341270b14b384daf2dfd11aac84
safayatalif <safayatalif2021@gmail.com>
2023-07-17 18:56:41 +0600

    create a categories section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,15 @@
-import Navbar from "../components/Shared/Navbar/Navbar"
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Shared/Navbar/Navbar'
+// import Footer from '../components/Shared/Footer/Footer'
 
 const Main = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      {/* 53m */}
+      <Navbar />
+      <div className='pt-28 pb-20'>
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
     </div>
   )
 }
