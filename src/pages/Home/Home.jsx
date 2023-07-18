3f691205ec3d17106e278ff5b2ccc0028358f3c8
safayatalif <safayatalif2021@gmail.com>
2023-07-18 17:06:44 +0600

    create a rooms section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Categories from '../../components/Categories/Categories'
-// import Rooms from '../../components/Rooms/Rooms'
+import Rooms from '../../components/Rooms/Rooms'
 
 const Home = () => {
   return (
     <>
       <Categories />
-      {/* <Rooms /> */}
+      <Rooms />
     </>
   )
 }
